c7fb6c4ae84c0a3cc97213bf8553a8ae930d16af
MikeCoder <mikecoder13@gmail.com>
2014-03-12 14:07:22 +0800

    init mike's work

---
+++ alipaycontest/src/iot/mike/alipaycontest/firstseason/DataHolder.java
@@ -0,0 +1,12 @@
+package iot.mike.alipaycontest.firstseason;
+
+/**
+ * This class used to hold the data instead of get data 
+ * from the database each time.
+ * 
+ * @author mike
+ *
+ */
+public class DataHolder {
+
+}
